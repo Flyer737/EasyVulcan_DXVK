060cbe920e36bc3cda04ddfa5eadf2411b2a29e6
Flyer737 <69780367+Flyer737@users.noreply.github.com>
2020-12-28 17:02:12 -0700

    Update Ubuntu_nvidia-DXVK_installer.sh
    Fixed wine thingy

--- Ubuntu_nvidia-DXVK_installer.sh
+++ Ubuntu_nvidia-DXVK_installer.sh
@@ -1,4 +1,8 @@
+sudo dpkg --add-architecture i386
+
 #wine
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
 
 #Ubuntu 20.04
 sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
@@ -26,7 +30,6 @@ sudo apt-get install --install-recommends winehq-stable
 #sudo apt-get install libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386
 
 sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install nvidia-driver-450
 sudo apt install libvulkan1 libvulkan1:i386
